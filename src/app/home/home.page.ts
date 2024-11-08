8ae628e18ca96a71752cb0e548686d459e9b3816
Pablo <pmacsal2707@g.educaand.es>
2024-11-08 10:24:17 +0100

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  users: any;
+
+  constructor(private httpClient: HttpClient) {
+    this.users = this.httpClient.get('https://randomuser.me/api/?results=100').pipe(map((res:any) => res['results']));
+  }
+
+}
